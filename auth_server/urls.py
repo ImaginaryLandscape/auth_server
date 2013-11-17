78730d5d88386300d2d6267b155ee6919c422906
Joe Jasinski <joe.jasinski@gmail.com>
2013-11-17 10:38:05 -0600

    cleanup

--- auth_server/urls.py
+++ auth_server/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import patterns, include, url
 from .views import AuthView
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', AuthView.as_view(), name='auth'),
-    # url(r'^/', include('.foo.urls')),
-
 )
+
